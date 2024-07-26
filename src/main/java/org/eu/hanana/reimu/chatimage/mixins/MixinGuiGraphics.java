4a0ec7f382a63e8bc027716e139844d3393df079
fhyjs <fenhyjs@163.com>
2024-07-26 15:03:09 +0800

    click

--- src/main/java/org/eu/hanana/reimu/chatimage/mixins/MixinGuiGraphics.java
+++ src/main/java/org/eu/hanana/reimu/chatimage/mixins/MixinGuiGraphics.java
@@ -2,9 +2,11 @@ package org.eu.hanana.reimu.chatimage.mixins;
 
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.chat.ClickEvent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.HoverEvent;
 import net.minecraft.network.chat.Style;
+import org.eu.hanana.reimu.chatimage.ChatimageMod;
 import org.eu.hanana.reimu.chatimage.client.RenderCi;
 import org.eu.hanana.reimu.chatimage.core.Actions;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,8 +14,11 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import javax.annotation.Nullable;
+
+import static net.minecraft.client.gui.screens.Screen.hasShiftDown;
 
 @Mixin(GuiGraphics.class)
 public abstract class MixinGuiGraphics {
@@ -27,4 +32,5 @@ public abstract class MixinGuiGraphics {
             }
         }
     }
+
 }
--- src/main/java/org/eu/hanana/reimu/chatimage/mixins/MixinScreen.java
+++ src/main/java/org/eu/hanana/reimu/chatimage/mixins/MixinScreen.java
@@ -4,11 +4,19 @@ import net.minecraft.client.gui.components.Renderable;
 import net.minecraft.client.gui.components.events.AbstractContainerEventHandler;
 import net.minecraft.client.gui.screens.ChatScreen;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.ClickEvent;
+import net.minecraft.network.chat.Style;
+import org.eu.hanana.reimu.chatimage.ChatimageMod;
+import org.eu.hanana.reimu.chatimage.core.Actions;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import javax.annotation.Nullable;
+
+import static net.minecraft.client.gui.screens.Screen.hasShiftDown;
 
 @Mixin(Screen.class)
 public abstract class MixinScreen extends AbstractContainerEventHandler implements Renderable {
@@ -20,6 +28,22 @@ public abstract class MixinScreen extends AbstractContainerEventHandler implemen
             }
             callbackInfo.setReturnValue(super.keyPressed(pKeyCode, pScanCode, pModifiers));
             callbackInfo.cancel();
+        }
+    }
+    @Inject(method = "handleComponentClicked(Lnet/minecraft/network/chat/Style;)Z",at=@At("RETURN"))
+    public void handleComponentClicked(@Nullable Style pStyle, CallbackInfoReturnable<Boolean> callbackInfo){
+        if (pStyle == null) {
+            return;
+        }
+        ClickEvent clickevent = pStyle.getClickEvent();
+        if (hasShiftDown()) {
+            return;
+        }
+        if (clickevent != null) {
+            if (clickevent.getAction() == Actions.getViewImage()) {
+                ChatimageMod.logger.info("But the Chatimage Mod knows!");
+                //TODO: 显示详情
+            }
         }
     }
 }
